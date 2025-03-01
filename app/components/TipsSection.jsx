879432848a9a1045d8a904e773bc215925094271
Viktor Makarskyy <>
2025-02-28 22:14:42 -0800

    Improve tips

--- app/components/TipsSection.jsx
+++ app/components/TipsSection.jsx
@@ -6,13 +6,12 @@ export default function TipsSection() {
         "By default, the icon will be colorful, ask for a 'line art icon' to simplify",
         "Clear between new icon requests",
         "Ask for thinner or thicker lines",
-        "Specify hex colors in your prompt",
         "If you get an error, click retry",
         "Try asking for animations or gradients",
         "Keep trying if you don't like the first result",
         "Ask to change theme or style",
         "Ask to remove specific things you don't like",
-        "Using keyboard shortcuts is highly recommended",
+        "Using keyboard shortcuts is recommended",
         "Paste SVG into Figma for fine-tune edits",
     ];
 
